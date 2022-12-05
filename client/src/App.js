055ebfdfa7c1e78fbb07ca76be1b5e032e97ab14
SriKarthikeya23 <karthikeyasri13@gmail.com>
2022-12-05 14:52:15 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,188 @@
+import Axios from 'axios';
+import './App.css';
+import React, { useState } from 'react';
+
+function App() {
+  
+  const [val1, setVal1] = useState(false);
+  const [val2, setVal2] = useState(false);
+  const [val3, setVal3] = useState(false);
+  const [val4, setVal4] = useState(false);
+
+  var light1 = 0;
+  var light2 = 0;
+  var light3 = 0;
+  var light4 = 0;
+  
+  const getLightData = () => {
+    Axios.get('http://localhost:3001/get/light/all').then((res) => {
+      light1 = res.data.light1;
+      light2 = res.data.light2;
+      light3 = res.data.light3;
+      light4 = res.data.light4;
+      lightState1(light1);
+      lightState2(light2);
+      lightState3(light3);
+      lightState4(light4);
+      })
+    }
+
+    const lightState1 = (val) => {
+      if(val === 1) {
+        setVal1(true);
+      } else if(val === 0) {
+        setVal1(false);
+      }
+    }
+    const lightState2 = (val) => {
+      if(val === 1) {
+        setVal2(true);
+      } else if(val === 0) {
+        setVal2(false);
+      }
+    }
+    const lightState3 = (val) => {
+      if(val === 1) {
+        setVal3(true);
+      } else if(val === 0) {
+        setVal3(false);
+      }
+    }
+    const lightState4 = (val) => {
+      if(val === 1) {
+        setVal4(true);
+      } else if(val === 0) {
+        setVal4(false);
+      }
+    }
+
+    const postLight1 = (state) => {
+      Axios.post("http://localhost:3001/post/light/1", {
+        data: state
+      }).then(() => {
+        console.log("Light 1 status sent");
+      })
+    }
+  
+    const postLight2 = (state) => {
+      Axios.post("http://localhost:3001/post/light/2", {
+        data: state
+      }).then(() => {
+        console.log("Light 2 status sent");
+      })
+    }
+  
+    const postLight3 = (state) => {
+      Axios.post("http://localhost:3001/post/light/3", {
+        data: state
+      }).then(() => {
+        console.log("Light 3 status sent");
+      })
+    }
+  
+    const postLight4 = (state) => {
+      Axios.post("http://localhost:3001/post/light/4", {
+        data: state
+      }).then(() => {
+        console.log("Light 4 status sent");
+      })
+    }
+
+  const toggler1 = () => {
+    if(val1 === true) {
+      setVal1(false);
+      postLight1(0);
+    } else {
+      setVal1(true);
+      postLight1(1);
+    }
+  }
+  const toggler2 = () => {
+    if(val2 === true) {
+      setVal2(false);
+      postLight2(0);
+    } else {
+      setVal2(true);
+      postLight2(1);
+    }
+  }
+  const toggler3 = () => {
+    if(val3 === true) {
+      setVal3(false);
+      postLight3(0);
+    } else {
+      setVal3(true);
+      postLight3(1);
+    }
+  }
+  const toggler4 = () => {
+    if(val4 === true) {
+      setVal4(false);
+      postLight4(0);
+    } else {
+      setVal4(true);
+      postLight4(1);
+    }
+  }
+
+  const [ct1, setCt1] = useState(0);
+  const [ct2, setCt2] = useState(0);
+  const [ct3, setCt3] = useState(0);
+  const [ct4, setCt4] = useState(0);
+
+  const getCtData = () => {
+    Axios.get("http://localhost:3001/get/ct/all").then((response) => {
+      setCt1(response.data.ct1);
+      setCt2(response.data.ct2);
+      setCt3(response.data.ct3);
+      setCt4(response.data.ct4);
+    })
+  };
+
+  setInterval(getLightData, 5000);
+  setInterval(getCtData, 10000);
+
+  return (
+    <div className='App'>
+      <h1>Smart Street Light</h1>
+
+      <div className='light-control'>
+        <h1>Light Control</h1>
+
+        <h3>Light 1</h3>
+        <label className="switch" >
+          <input type="checkbox" checked={val1} onClick={toggler1}></input>
+          <span className="slider round"></span>
+        </label>
+
+        <h3>Light 2</h3>
+        <label className="switch" >
+          <input type="checkbox" checked={val2} onClick={toggler2}></input>
+          <span className="slider round"></span>
+        </label>
+
+        <h3>Light 3</h3>
+        <label className="switch" >
+          <input type="checkbox" checked={val3} onClick={toggler3}></input>
+          <span className="slider round"></span>
+        </label>
+
+        <h3>Light 4</h3>
+        <label className="switch" >
+          <input type="checkbox" checked={val4} onClick={toggler4}></input>
+          <span className="slider round"></span>
+        </label>
+      </div>
+
+      <div className='ct'>
+        <h2>CT values</h2>
+        <p>CT Value 1 = {ct1}</p>
+        <p>CT Value 2 = {ct2}</p>
+        <p>CT Value 3 = {ct3}</p>
+        <p>CT Value 4 = {ct4}</p>
+      </div>
+    </div>
+ );
+}
+
+export default App
---
+++ server/index.js
@@ -0,0 +1,146 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const mqtt = require('mqtt');
+
+app.use(cors());
+app.use(express.json());
+
+var data1 = 0;
+var data2 = 0;
+var data3 = 0;
+var data4 = 0;
+var light_temp1 = 0;
+var light_temp2 = 0;
+var light_temp3 = 0;
+var light_temp4 = 0;
+var light1 = mqtt.connect('mqtt://35.234.254.79:1883');
+var light2 = mqtt.connect('mqtt://35.234.254.79:1883');
+var light3 = mqtt.connect('mqtt://35.234.254.79:1883');
+var light4 = mqtt.connect('mqtt://35.234.254.79:1883');
+
+app.post('/post/light/1', (req, res) => {
+    data1 = req.body.data;
+    console.log("Light 1 "+data1);
+    res.send("received data light 1");
+});
+light1.on("connect", function() {
+    setInterval(function() {
+        if(data1 != light_temp1) {
+            light1.publish("street/l1", JSON.stringify(data1));
+            light_temp1 = data1;
+        }
+    }),1000;
+});
+
+
+app.post('/post/light/2', (req, res) => {
+    data2 = req.body.data;
+    console.log("light 2 "+data2);
+    res.send("received data light 2");
+});
+light2.on("connect", function() {
+    setInterval(function() {
+        if(data2 != light_temp2) {
+            light2.publish("street/l2", JSON.stringify(data2));
+            light_temp2 = data2;
+        }
+    }),1000;
+});
+
+app.post('/post/light/3', (req, res) => {
+    data3 = req.body.data;
+    console.log("light 3 "+data3);
+    res.send("received data light 3");
+});
+light3.on("connect", function() {
+    setInterval(function() {
+        if(data3 != light_temp3) {
+            light3.publish("street/l3", JSON.stringify(data3));
+            light_temp3 = data3;
+        }
+    }),1000;
+});
+
+app.post('/post/light/4', (req, res) => {
+    data4 = req.body.data;
+    console.log("light 4 "+ data4);
+    res.send("received data light 4");
+});
+light4.on("connect", function() {
+    setInterval(function() {
+        if(data4 != light_temp4) {
+            light4.publish("street/l4", JSON.stringify(data4));
+            light_temp4 = data4;
+        }
+    }),1000;
+});
+
+app.get('/get/light/all', (req, res) => {
+    var data = {
+        "light1" : data1,
+        "light2" : data2,
+        "light3" : data3,
+        "light4" : data4
+    }
+    res.send(data);
+});
+
+var ct1 = mqtt.connect('mqtt://35.234.254.79:1883');
+var ct2 = mqtt.connect('mqtt://35.234.254.79:1883');
+var ct3 = mqtt.connect('mqtt://35.234.254.79:1883');
+var ct4 = mqtt.connect('mqtt://35.234.254.79:1883');
+var ctdata1 = 0;
+var ctdata2 = 0;
+var ctdata3 = 0;
+var ctdata4 = 0;
+
+ct1.on('connect', function() {
+    ct1.subscribe("street/ct1");
+    console.log("subscribed to ct 1")
+});
+ct1.on('message', function(topic, message) {
+    console.log("CT 1 Data received : "+ message.toString());
+    ctdata1 = parseInt(message.toString());
+});
+
+ct2.on('connect', function() {
+    ct2.subscribe("street/ct2");
+    console.log("subscribed to ct 2")
+});
+ct2.on('message', function(topic, message) {
+    console.log("CT 2 Data received : "+ message.toString());
+    ctdata2 = parseInt(message.toString());
+});
+
+ct3.on('connect', function() {
+    ct3.subscribe("street/ct3");
+    console.log("subscribed to ct 3")
+});
+ct3.on('message', function(topic, message) {
+    console.log("CT 3 Data received : "+ message.toString());
+    ctdata3 = parseInt(message.toString());
+});
+
+ct4.on('connect', function() {
+    ct4.subscribe("street/ct4");
+    console.log("subscribed to ct 4")
+});
+ct4.on('message', function(topic, message) {
+    console.log("CT 4 Data received : "+ message.toString());
+    ctdata4 = parseInt(message.toString());
+});
+
+app.get('/get/ct/all', (req, res) => {
+    var data = {
+        "ct1" : ctdata1,
+        "ct2" : ctdata2,
+        "ct3" : ctdata3,
+        "ct4" : ctdata4
+    }
+    res.send(data);
+});
+
+app.listen(3001, () => {
+    console.log("running on port 3001");
+})
